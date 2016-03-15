19c44f95b01769d218af19e1a29ee8a27a703dac
Laurier2013 <luominyn@hotmail.com>
2016-03-15 13:12:21 -0400

    Update Luo Min DML.sql

--- Luo Min DML.sql
+++ Luo Min DML.sql
@@ -10,6 +10,16 @@ INSERT INTO Customer_T  (CustomerID, Cust_Fname, Cust_Lname, Cust_Phone, Cust_Ad
 VALUES ('C004', 'Edward', 'Ford', '6045562173', '935 Marine Dr', 'Vancouver', 'BC', 'V7P1S0',1);
 INSERT INTO Customer_T  (CustomerID, Cust_Fname, Cust_Lname, Cust_Phone, Cust_Address, Cust_City, Cust_Prov, Cust_PostCode, Cust_Membership)
 VALUES ('C005', 'Kate', 'White', '6045231892', ' 3292 E 29th Ave', 'Vancouver', 'BC', 'V5R1W4',1);
+INSERT INTO Customer_T  (CustomerID, Cust_Fname, Cust_Lname, Cust_Phone, Cust_Address, Cust_City, Cust_Prov, Cust_PostCode, Cust_Membership)
+VALUES('C011','Tom', 'Handerson', '6138076624', '73 Carlings Ave', 'Ottawa', 'ON', 'K2V7K9',0);
+INSERT INTO Customer_T  (CustomerID, Cust_Fname, Cust_Lname, Cust_Phone, Cust_Address, Cust_City, Cust_Prov, Cust_PostCode, Cust_Membership)
+VALUES('C012','Marry', 'Karry', '8190072356', 'PO BOX 12', 'Quebec', 'QC', 'H2A1B2',0);
+INSERT INTO Customer_T  (CustomerID, Cust_Fname, Cust_Lname, Cust_Phone, Cust_Address, Cust_City, Cust_Prov, Cust_PostCode, Cust_Membership)
+VALUES('C013','Alexander','Simons', '4160129564', '100 Young Street', 'Toronto', 'ON', 'R3B2S4', 1);
+INSERT INTO Customer_T  (CustomerID, Cust_Fname, Cust_Lname, Cust_Phone, Cust_Address, Cust_City, Cust_Prov, Cust_PostCode, Cust_Membership)
+VALUES('C014','Alton', 'John', '4161546584', '68 Grangeway Ave', 'Toronto', 'ON', 'M1H0A1', 1);
+INSERT INTO Customer_T  (CustomerID, Cust_Fname, Cust_Lname, Cust_Phone, Cust_Address, Cust_City, Cust_Prov, Cust_PostCode, Cust_Membership)
+VALUES('C015','Harry', 'Tang', '4167508262', '1470 Midland Ave','Toronto', 'ON','M2H1A3', 1);
 
 -- Populate Author_T
 #TRUNCATE TABLE Author_T;
@@ -23,6 +33,17 @@ INSERT INTO Author_T (AuthorID, Author_Name, BookID)
 VALUES('A004', 'Michael Dobbs', 'B004');
 INSERT INTO Author_T (AuthorID, Author_Name, BookID)
 VALUES('A005', 'Danielle Dutton', 'B005');
+INSERT INTO Author_T (AuthorID, Author_Name, BookID)
+VALUES('A011', 'Alan Anderson', 'B011');
+INSERT INTO Author_T (AuthorID, Author_Name, BookID)
+VALUES('A012', 'David Pogue', 'B012');
+INSERT INTO Author_T (AuthorID, Author_Name, BookID)
+VALUES('A013', 'Bruce Elbert', 'B013');
+INSERT INTO Author_T (AuthorID, Author_Name, BookID)
+VALUES('A014', 'Lincoln Peirce', 'B014');
+INSERT INTO Author_T (AuthorID, Author_Name, BookID)
+VALUES('A015', 'Suzanne Collins', 'B015');
+
 
 -- Populate Book_T
 #TRUNCATE TABLE Book_T;
@@ -36,6 +57,16 @@ INSERT INTO Book_T(BookID, AuthorID, Title, ISBN, Category,ReleaseYear, Publishe
 VALUES('B004', 'A004', 'House of Cards', '1492606618' , 'Literature', '2013', 'Simon & Schuster UK');
 INSERT INTO Book_T(BookID, AuthorID, Title, ISBN, Category, ReleaseYear, Publisher)
 VALUES('B005', 'A005','Margaret the First: A Novel','1936787350','Literature', '2015', 'Catapult');
+INSERT INTO Book_T(BookID, AuthorID, Title, ISBN, Category, Price, ReleaseYear, Publisher)
+VALUES('B011', 'A011', 'Exploratory Data Mining and Data Cleaning', '0471268518', 'TextBook', '157.99', '2015', 'Wiley Interscience');
+INSERT INTO Book_T(BookID, AuthorID, Title, ISBN, Category, Price, ReleaseYear, Publisher)
+VALUES('B012', 'A012', 'Windows 10: The Missing Manual', '1491947179', 'TextBook', '33.54', '2015', "O'Reilly Medi");
+INSERT INTO Book_T(BookID, AuthorID, Title, ISBN, Category, Price, ReleaseYear, Publisher)
+VALUES('B013', 'A013', 'The Satellite Communication and Earth Station Handbook','1608076733', 'TextBook', '234.40', '2015', 'Artech House Publishers');
+INSERT INTO Book_T(BookID, AuthorID, Title, ISBN, Category, Price, ReleaseYear, Publisher)
+VALUES('B014', 'A014', 'Big Nate', '1449462278', 'Children', '8.00', '2015', 'Disney Hyperion');
+INSERT INTO Book_T(BookID, AuthorID, Title, ISBN, Category, Price, ReleaseYear, Publisher)
+VALUES('B015', 'A015', 'The Hunger Games', '0439023483', 'Children', '15.45', '2015', 'Scholastic Press');
 
 -- Populate Inventory_T
 #TRUNCATE TABLE Inventory_T;
